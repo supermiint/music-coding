77390e6a5106ef9a4c8857ceade26c1dda498814
Anocha Upontian <anocha.upon@gmail.com>
2019-10-21 00:11:54 +0700

    twinkle song

---
+++ twnkle.rb
@@ -0,0 +1,87 @@
+# default bpm = 60
+# TWINKLE TWINKLE LITTLE STAR
+use_synth :beep
+play 48 #C3
+sleep 0.5 # timing 0.5 beat on 60 beats per minute
+play 48 #C3
+sleep 0.5
+play 55 #G3
+sleep 0.5
+play 55 #G3
+sleep 0.5
+play 57 #A3
+sleep 0.5
+play 57 #A3
+sleep 0.5
+play 55 #G3
+sleep 1 # timing 1 beat on 60 beats per minute
+play 53 #F3
+sleep 0.5
+play 53 #F3
+sleep 0.5
+play 52 #E3
+sleep 0.5
+play 52 #E3
+sleep 0.5
+play 50 #D3
+sleep 0.5
+play 50 #D3
+sleep 0.5
+play 48 #C3
+sleep 1
+play 55 #G3
+sleep 0.5
+play 55 #G3
+sleep 0.5
+play 53 #F3
+sleep 0.5
+play 53 #F3
+sleep 0.5
+play 52 #E3
+sleep 0.5
+play 52 #E3
+sleep 0.5
+play 50 #D3
+sleep 1
+play 55 #G3
+sleep 0.5
+play 55 #G3
+sleep 0.5
+play 53 #F3
+sleep 0.5
+play 53 #F3
+sleep 0.5
+play 52 #E3
+sleep 0.5
+play 52 #E3
+sleep 0.5
+play 50 #D3
+sleep 1
+play 48 #C3
+sleep 0.5
+play 48 #C3
+sleep 0.5
+play 55 #G3
+sleep 0.5
+play 55 #G3
+sleep 0.5
+play 57 #A3
+sleep 0.5
+play 57 #A3
+sleep 0.5
+play 55 #G3
+sleep 1
+play 53 #F3
+sleep 0.5
+play 53 #F3
+sleep 0.5
+play 52 #E3
+sleep 0.5
+play 52 #E3
+sleep 0.5
+play 50 #D3
+sleep 0.5
+play 50 #D3
+sleep 0.5
+play 48 #C3
+sleep 1
